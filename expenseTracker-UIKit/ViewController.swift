6ddba549d6fdb5160d427de9b19ed1e1486c91de
silthice <ryonlim@hotmail.com>
2023-03-10 10:03:02 +0800

    Initial setup

---
+++ expenseTracker-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  expenseTracker-UIKit
+//
+//  Created by Giap on 10/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
